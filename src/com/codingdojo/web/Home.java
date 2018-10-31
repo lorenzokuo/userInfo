a4d8360ab3fe90f9245435cb3e4dba8aeeb1ffb0
Lorenzokuo <lorenzokuo@gmail.com>
2018-10-30 23:01:22 -0700

    first commit

---
+++ src/com/codingdojo/web/Home.java
@@ -0,0 +1,55 @@
+package com.codingdojo.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		// get the value for the query parameter
+		String userName = request.getParameter("name");
+		String userLanguage = request.getParameter("language");
+		String userLocation = request.getParameter("location");
+        response.setContentType("text/html");
+        // set user location as unknown
+        userLocation = "Unknown";
+        PrintWriter out = response.getWriter();
+        out.write("<h1>Welcome, " + userName + "</h1>");
+        out.write("Your favorite language is: , " + userLanguage);
+        out.write("<p>Your hometown is: " + userLocation + "</p>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
